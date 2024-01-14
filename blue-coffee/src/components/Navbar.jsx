55857caf05c654eb8bd563b128b7b5425acaec95
Soyeon Kim <kksoyeon61@gmail.com>
2024-01-14 20:06:00 +0100

    create navbar

---
+++ blue-coffee/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+
+function Navbar () {
+    return(
+        <div>
+            <title className="header">Blue Coffee</title>
+            <ul className="navbar-list">
+                <li className="navbar-items">ABOUT</li>
+                <li className="navbar-items">MENU</li>
+                <li className="navbar-items">COMPANY</li>
+            </ul>
+        </div>
+    )
+}
+export default Navbar;
