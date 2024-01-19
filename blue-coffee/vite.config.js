1d5f361076a012d23b939915b9c54647de99f74c
Soyeon Kim <kksoyeon61@gmail.com>
2024-01-19 22:04:16 +0100

    add via upload

--- blue-coffee/vite.config.js
+++ blue-coffee/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import nodeResolve from '@rollup/plugin-node-resolve';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: path.join(__dirname, 'src'),
-  plugins: [react()]
+  root: path.resolve(__dirname, 'src'),
+  plugins: [react(),nodeResolve()],
 
 })
